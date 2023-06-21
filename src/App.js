4ae5812b9e4133488a942ced842446aef33bbe3c
Amaan Jambura <amaanjambura786@gmail.com>
2023-06-21 23:07:48 +0530

    Added required routes for linking the home screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
+
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Layout />
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
   return (
     <div className="App">
-      {/* <Login/> */}
-      <Register/>
+       <RouterProvider router={router} />
     </div>
   );
 }
