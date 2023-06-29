6d1d357f87350f742d7e3dcf414548d0292f08bc
Amaan Jambura <amaanjambura786@gmail.com>
2023-06-29 19:28:49 +0530

    Added and completed User posts ui design

---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,32 @@
+import Post from "../post/Post";
+import "./posts.scss";
+
+const Posts = () => {
+  const posts = [
+    {
+      id: 1,
+      name: "Jane Doe",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 2,
+      name: "Chloe Doe",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/1036622/pexels-photo-1036622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+  ];
+
+  return <div className="posts">
+    {posts.map(post=>(
+      <Post post={post} key={post.id}/>
+    ))}
+  </div>;
+};
+
+export default Posts;
